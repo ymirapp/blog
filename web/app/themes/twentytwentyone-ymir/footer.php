78d7678ee5e0e3dbd270418e829b6feae96807fd
Carl Alexander <carlalexander@gmail.com>
2021-10-13 23:46:49 -0400

    add free trial footer and remove publish date and category

---
+++ web/app/themes/twentytwentyone-ymir/footer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+?>
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div><!-- #content -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ web/app/themes/twentytwentyone-ymir/template-parts/content/content-single.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<header class="entry-header alignwide">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php twenty_twenty_one_post_thumbnail(); ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+		the_content();
+
+		wp_link_pages(
+			array(
+				'before'   => '<nav class="page-links" aria-label="' . esc_attr__( 'Page', 'twentytwentyone' ) . '">',
+				'after'    => '</nav>',
+				/* translators: %: Page number. */
+				'pagelink' => esc_html__( 'Page %', 'twentytwentyone' ),
+			)
+		);
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer default-max-width">
+        <p class="is-large-text">Stop worrying about your WordPress server</p>
+        <a class="button" style="text-align: center" href="https://ymirapp.com/register">Start your 14-day trial</a>
+	</footer><!-- .entry-footer -->
+
+</article><!-- #post-<?php the_ID(); ?> -->
