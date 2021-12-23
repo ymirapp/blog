7a902f804974fbfab3753914a468a6699d42b107
Carl Alexander <carlalexander@gmail.com>
2021-12-23 12:03:15 -0500

    tweak post content

---
+++ web/app/themes/twentytwentyone-ymir/template-parts/content/content.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php if ( is_singular() ) : ?>
+			<?php the_title( '<h1 class="entry-title default-max-width">', '</h1>' ); ?>
+		<?php else : ?>
+			<?php the_title( sprintf( '<h2 class="entry-title default-max-width"><a href="%s">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<?php endif; ?>
+
+		<?php twenty_twenty_one_post_thumbnail(); ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+		the_content(
+			twenty_twenty_one_continue_reading_text()
+		);
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer default-max-width">
+	</footer><!-- .entry-footer -->
+</article><!-- #post-<?php the_ID(); ?> -->
